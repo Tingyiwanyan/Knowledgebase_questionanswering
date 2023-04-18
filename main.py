cb110880df59e51abc5799a857485dedf150a20f
Tingyi Wanyan <tingyi@Tingyis-MBP.attlocal.net>
2023-04-18 01:29:01 -0400

    correction

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from utility.infor_extraction import info_extractor
 if __name__ == '__main__':
 	info_ext = info_extractor()
 	text = "GDP in developing countries such as Vietnam will continue growing at a high rate." 
-	info_ext.subtree_matcher(text)
+	x,y,z = info_ext.subtree_matcher(text)
