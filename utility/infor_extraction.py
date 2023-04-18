e51a01decf25d553b6d606ab1c551a43a3bb2245
Tingyi Wanyan <tingyi@Tingyis-MBP.attlocal.net>
2023-04-18 01:14:48 -0400

    add dependency tree

--- utility/infor_extraction.py
+++ utility/infor_extraction.py
@@ -4,12 +4,50 @@ import numpy as np
 from spacy.matcher import Matcher
 
 
-nlp = spacy.load("en_core_web_sm")
+class info_extractor():
+  def __init__(self):
+    self.nlp = spacy.load("en_core_web_sm")
 
-text = "GDP in developing countries such as Vietnam will continue growing at a high rate." 
+    #text = "GDP in developing countries such as Vietnam will continue growing at a high rate." 
 
 # create a spaCy object 
-doc = nlp(text)
+  def subtree_matcher(text):
 
-for tok in doc: 
-  print(tok.text, "-->",tok.dep_,"-->", tok.pos_)
+    doc = nlp(text)
+
+    subjpass = 0
+
+    for i,tok in enumerate(doc):
+      # find dependency tag that contains the text "subjpass"    
+      if tok.dep_.find("subjpass") == True:
+        subjpass = 1
+
+    x = ''
+    y = ''
+    z = ''
+
+    # if subjpass == 1 then sentence is passive
+    if subjpass == 1:
+      for i,tok in enumerate(doc):
+        if tok.dep_.find("subjpass") == True:
+          y = tok.text
+
+        if tok.dep_.endswith("obj") == True:
+          x = tok.text
+
+        if tok.dep_.find("ROOT") == True:
+          z = tok.text
+    
+    # if subjpass == 0 then sentence is not passive
+    else:
+      for i,tok in enumerate(doc):
+        if tok.dep_.endswith("subj") == True:
+          x = tok.text
+
+        if tok.dep_.endswith("obj") == True:
+          y = tok.text
+
+        if tok.dep_.find("ROOT") == True:
+          z = tok.text
+
+    return x,y,z
