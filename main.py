89b6e429053adbae3d51353dbd8d7e269472d55b
Tingyi Wanyan <tingyi@Tingyis-MacBook-Pro.local>
2023-04-20 02:09:39 -0400

    adding sentence

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
 	info_ext = info_extractor()
 	text = "GDP in developing countries such as Vietnam will continue growing at a high rate." 
 	text2 = "Tableau was recently acquired by Salesforce." 
+	text3 = "AnalyticsVidhya is the largest community of data scientists and provides best resources for understanding data and analytics."
 	x,y,z = info_ext.subtree_matcher(text)
