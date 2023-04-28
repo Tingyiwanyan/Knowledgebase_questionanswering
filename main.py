69c54b4f1da09ace2956bb1628f820a82ab0e20d
Tingyi Wanyan <tingyi@Tingyis-MBP.lan>
2023-04-28 15:09:14 -0500

    correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utility.infor_extraction import info_extractor
 from spacy import displacy
+from utility.text_kg_generation import kg_construct
 
 
 if __name__ == '__main__':
@@ -12,4 +13,9 @@ if __name__ == '__main__':
 	text6 = "james and lucy are friends"
 	text7 = "moved by james, lucy went out"
 	text8 = "The accident happened as the night was falling"
+	text9 = "what is watercycle"
+	text10 = "john scored 98"
+	sample_url = "https://s22.q4cdn.com/959853165/files/doc_financials/2021/q4/da27d24b-9358-4b5c-a424-6da061d91836.pdf"
+
+	kg = kg_construct()
 	#info_ext.entity_matcher()
